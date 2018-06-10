8d744e7d66b241f1e0c7f8bb0eedd963de8550c1
m-eagan <32804934+M-Eagan@users.noreply.github.com>
2018-06-10 11:19:49 -0400

    Add files via upload
    Exercise 1 completed

---
+++ script.js
@@ -0,0 +1,12 @@
+//Exercsie 1
+var numGame = Number(prompt("Enter a number!"));
+
+var output = 0;
+var i= 0;
+for (i = 0; i <= numGame; i++) {
+  output += i;
+}
+
+console.log(output);
+
+//Exercise 2
